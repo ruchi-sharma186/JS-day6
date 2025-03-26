864587a0069ec12050034615606dd8c4288cec6b
ruchi-sharma186 <ruchisharmasharma216@gmail.com>
2025-03-26 22:13:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+let str = "heyy....sab log camera on kar lo.... camera.....";
+
+//let res = str.indexOf("camera");
+//let res = str.lastIndexOf("camera");
+let res = str.indexOf("camera" , 20);
+console.log(res);
+
+//string concatenation
+console.log("string concatenation");
+
+var string1 = "prep";
+var string2 = "Bytes";
+var string3 = string1 + string2;
+console.log(string3);
+
+//main method of string concatenation
+console.log("main method of string concatenation");
+
+let str1 = "hello";
+let str2 = "world";
+ let res1 = str1.concat(str2);
+ console.log(res1);
+
+ // Slicing
+ console.log("slice()");
+ 
+let string = "heyy....sab log camera on kar lo.... camera.....";
+//let result = str.slice(10, 25);
+let result = str.slice(25, -15);
+console.log(result)
+
+//Substring
+console.log("Substring()");
+
+let str5 = "you will be an amazing develop";
+let str6 = str5.substring(5, 7);
+console.log(str6)
+
+
+
+//Replace
+console.log("Replace()");
+
+let str10= "sharma";
+//let res10 = str.replace(" ","/");
+let res11 = str10.replace("h","i");
+console.log(res11);
+
+//trim
+console.log("trim()");
+
+let stringb = "heyy....sab log camera on kar lo.... camera.....";
+let res4 = str.trim();
+console.log(res4);
+
+//split
+console.log("split()");
+
+let string5 = "heyy....sab log camera on kar lo.... camera.....";
+let res5 = str.split(" ");
+//let res5 = str.split("log");
+console.log(res5);
+
+
+
+
+ 
